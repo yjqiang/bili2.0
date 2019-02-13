c3e8106e4785779d430eadbf75dede5678eb68c2
yjqiang <13307130285@fudan.edu.cn>
2019-02-13 10:06:30 +0800

    shit api for storm is deprecated
    and I bring a new api for you

--- reqs/storm_raffle_handler.py
+++ reqs/storm_raffle_handler.py
@@ -1,3 +1,4 @@
+import utils
 from bili_global import API_LIVE
 
 
@@ -9,7 +10,7 @@ class StormRaffleHandlerReq:
         return json_rsp
 
     @staticmethod
-    async def join(user, raffle_id):
+    async def join_deprecated(user, raffle_id):
         url = f'{API_LIVE}/lottery/v1/Storm/join'
         data = {
             "id": raffle_id,
@@ -20,4 +21,12 @@ class StormRaffleHandlerReq:
             "csrf_token": user.dict_bili['csrf']
         }
         json_rsp = await user.bililive_session.request_json('POST', url, data=data, headers=user.dict_bili['pcheaders'])
+        return json_rsp
+   
+    @staticmethod
+    async def join(user, raffle_id):
+        temp_params = f'access_key={user.dict_bili["access_key"]}&actionKey={user.dict_bili["actionKey"]}&appkey={user.dict_bili["appkey"]}&build={user.dict_bili["build"]}&device={user.dict_bili["device"]}&id={raffle_id}&mobi_app={user.dict_bili["mobi_app"]}&platform={user.dict_bili["platform"]}&ts={utils.curr_time()}'
+        sign = user.calc_sign(temp_params)
+        url = f'{API_LIVE}/lottery/v1/Storm/join?{temp_params}&sign={sign}'
+        json_rsp = await user.bililive_session.request_json('POST', url, headers=user.dict_bili['appheaders'])
         return json_rsp
