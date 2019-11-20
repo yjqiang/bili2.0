f735062467f64090251010cb81ed94fc3b84e663
yjqiang <yjqiang1@gmail.com>
2019-11-20 15:15:42 +0800

    b站 nmsl wsnd

--- danmu/bili_danmu_monitor.py
+++ danmu/bili_danmu_monitor.py
@@ -15,6 +15,7 @@ from tasks.storm_raffle_handler import StormRaffleJoinTask
 from tasks.utils import UtilsTask
 from . import raffle_handler
 from utils import clear_whitespace
+import printer
 
 
 class DanmuPrinter(WsDanmuClient):
@@ -31,9 +32,7 @@ class DanmuRaffleMonitor(WsDanmuClient):
     # 全区广播:主播<%咩咩想喝甜奶盖%>的直播间得到了偶像恋人的回应，快去前往抽奖吧！
     # 娱乐区广播:<%阿a飘piao大草莓pizza%>送给<%雫るる_Official%>1个摩天大楼，点击前往TA的房间去抽奖吧
     # .+X>(?!.*X) 是匹配最后一个 X
-    NOTICE_MSG_TV_PATTERN = r = re.compile(r'([^：:]+)广播'  # 广播类型
-                                           # 匹配最后一个 %>
-                                           r'.+%>(?!.*%>)'
+    NOTICE_MSG_TV_PATTERN = r = re.compile(r'.+%>(?!.*%>)'  # 匹配最后一个 %>
                                            # X个、XXX了、直播间XX了、暴力保留，
                                            # ⚠还有匹配的优先级（因为第二个和第三个可能混合"开启了XX直播间抽奖"（我自己造的））
                                            r'(?:(\d+)个|[^，,了]+了|[^，,]+?(?:直播间|房间)[^，,了]{2}了?)?'
@@ -98,12 +97,14 @@ class DanmuRaffleMonitor(WsDanmuClient):
             real_roomid = data['real_roomid']
             msg_common = clear_whitespace(data['msg_common'], '“”')
             if msg_type == 2 or msg_type == 8:
-                broadcast, description0, raffle_name = self.NOTICE_MSG_TV_PATTERN.match(msg_common).group(1, 2, 3)
+                description0, raffle_name = self.NOTICE_MSG_TV_PATTERN.match(msg_common).group(1, 2)
+                broadcast = 'nmb'
                 raffle_num = int(description0) if description0 is not None else 1
                 print(f'{self._area_id}号数据连接检测到{real_roomid:^9}的{raffle_name}')
                 raffle_handler.push2queue(TvRaffleJoinTask, real_roomid)
                 broadcast_type = 0 if broadcast == '全区' else 1
                 bili_statistics.add2pushed_raffles(raffle_name, broadcast_type, raffle_num)
+                # printer.warn(data['link_url'], msg_common)
             elif msg_type == 3:
                 raffle_name = self.NOTICE_MSG_GUARD_PATTERN.match(msg_common).group(1)
                 print(f'{self._area_id}号数据连接检测到{real_roomid:^9}的{raffle_name}')
