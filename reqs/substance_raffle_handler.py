5a40d3c14b81256bd68d8417b898f9981882eac4
mengshouer <mengshouer@outlook.com>
2021-02-13 19:59:38 +0800

    change substance api to v2

--- reqs/substance_raffle_handler.py
+++ reqs/substance_raffle_handler.py
@@ -4,21 +4,21 @@ from bili_global import API_LIVE
 class SubstanceRaffleHandlerReq:
     @staticmethod
     async def check(user, aid):
-        url = f"{API_LIVE}/lottery/v1/box/getStatus?aid={aid}"
+        url = f"{API_LIVE}/xlive/lottery-interface/v2/Box/getStatus?aid={aid}"
         # {"code":-500,"msg":"未登录","message":"未登录","data":[]}
         json_rsp = await user.bililive_session.request_json('GET', url, headers=user.pc.headers)
         return json_rsp
 
     @staticmethod
     async def join(user, aid, number):
-        url = f'{API_LIVE}/lottery/v1/box/draw?aid={aid}&number={number}'
+        url = f'{API_LIVE}/xlive/lottery-interface/v2/Box/draw?aid={aid}&number={number}'
         # {"code":-500,"msg":"未登录","message":"未登录","data":[]}
         json_rsp = await user.bililive_session.request_json('GET', url, headers=user.pc.headers)
         return json_rsp
 
     @staticmethod
     async def notice(user, aid, number):
-        url = f"{API_LIVE}/lottery/v1/box/getWinnerGroupInfo?aid={aid}&number={number}"
+        url = f"{API_LIVE}/xlive/lottery-interface/v2/Box/getWinnerGroupInfo?aid={aid}&number={number}"
         # {"code":-500,"msg":"未登录","message":"未登录","data":[]}
         json_rsp = await user.bililive_session.request_json('GET', url, headers=user.pc.headers)
         return json_rsp
